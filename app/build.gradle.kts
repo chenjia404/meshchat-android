b2f6f7a1e7063e73293e2c6144e49007b6f81b6d
chenjia404 <chenjiablog@gmail.com>
2026-03-25 10:01:30 +0800

    1.0.4：添加离线消息

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.github.com.chenjia404.meshchat"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
